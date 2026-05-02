5274daba8305e476f3aff1b1bdf15a062d7f1b9a
Adbuth Edits <adbuthedits@gmail.com>
2026-05-02 17:18:04 +0530

    Fix: Add database sync on startup to initialize tables

---
+++ backend/sync_db.js
@@ -0,0 +1,17 @@
+const sequelize = require('./config/database');
+const models = require('./models');
+
+const syncDatabase = async () => {
+    try {
+        console.log('Connecting to database and syncing tables...');
+        // This will create tables if they do not exist (and does nothing if they already exist)
+        await sequelize.sync({ alter: false }); 
+        console.log('Database tables synced successfully!');
+        process.exit(0);
+    } catch (err) {
+        console.error('Error syncing database:', err);
+        process.exit(1);
+    }
+};
+
+syncDatabase();
